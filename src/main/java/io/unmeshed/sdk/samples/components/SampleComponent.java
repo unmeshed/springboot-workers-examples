106e78848900aae3dabcea60f901cd3a1cc3ebe9
sauronmesh <saksham@unmeshed.com>
2026-02-08 20:50:36 +0530

    feat: Print current work Request

--- src/main/java/io/unmeshed/sdk/samples/components/SampleComponent.java
+++ src/main/java/io/unmeshed/sdk/samples/components/SampleComponent.java
@@ -1,13 +1,17 @@
 package io.unmeshed.sdk.samples.components;
 
+import io.unmeshed.client.WorkContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
+@Slf4j
 public class SampleComponent {
 
     public Map<String, Object> getSampleData() {
+        log.info("Current Work Request Details : {}", WorkContext.currentWorkRequest());
         return Map.of(
                 "message", "Hello World !!",
                 "success", true,
